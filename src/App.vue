fa1203a38c153207c3d2f55542d0d9db56b8b972
codezhangxile <61828835+codezhangxile@users.noreply.github.com>
2021-10-02 23:01:28 +0800

    router搭建

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
+  <div class="head-bar">
+    <router-link active-class="router-link-active" to="/">home</router-link>
+    <router-link active-class="router-link-active" to="/router">router</router-link>
+  </div>
 
 
+  <router-view />
 </template>
 
 <script lang="ts">
@@ -16,6 +21,24 @@ export default defineComponent({
 
 <style lang="scss" >
 #app{
+  .head-bar{
+    height: 40px;
+    a{
+      display: inline-block;
+      text-decoration: none;
+      width: 80px;
+      height: 40px;
+      line-height: 40px;
+      text-align: center;
+      border: 1px solid blue;
+      background-color: black;
+      color: #fff;
+    }
+    .router-link-active{
+      color: red;
+    }
+  }
+
 
 }
 </style>
